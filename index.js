858ab353114d758e04894654bd9e4a051bff28bf
leticia <leticia_junqueira10@notmail.com>
2025-01-23 21:23:25 -0300

    iniciando viagens

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+// const menuHamburguer = document.querySelector('.menu-hamburguer');
+// const navegacao = document.querySelector('.navegacao__lista'); // Selecione a lista de itens do menu
+
+menuHamburguer.addEventListener('click', () => {
+  navegacao.classList.toggle('active');
+});
